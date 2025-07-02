4e3b44ea695d1dddeb26aac7174e87c62d3666b5
jadencastle <jadencastle303@gmail.com>
2025-07-02 17:45:07 -0600

    Setting Up Base Site

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/jaden-portfolio/',
   plugins: [react(), tailwindcss()],
 })
